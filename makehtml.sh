0f25fb515287037d1df76fdf9fea18c9d213839d
Richard Siddall <richard.siddall@elirion.net>
2021-02-07 00:36:56 +0000

    Added a shebang to the make script.

--- makehtml.sh
+++ makehtml.sh
@@ -1,9 +1,8 @@
+#!/bin/sh
 asciidoc -a stylesheet=themes/pathways/style.css -a stylesdir=themes/pathways/ index.adoc >index.html
 asciidoc -a stylesheet=themes/pathways/style.css -a stylesdir=themes/pathways/ basics.adoc >basics.html
 asciidoc -a stylesheet=themes/pathways/style.css -a stylesdir=themes/pathways/ constraintsystems.adoc >constraintsystems.html
 asciidoc -a stylesheet=themes/pathways/style.css -a stylesdir=themes/pathways/ advanced.adoc >advanced.html
 asciidoc -a stylesheet=themes/pathways/style.css -a stylesdir=themes/pathways/ final.adoc >final.html
 asciidoc -a stylesheet=themes/pathways/style.css -a stylesdir=themes/pathways/ examples.adoc >examples.html
-
-
 
